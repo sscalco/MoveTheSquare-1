eb28533b44f0b1baf090ada9eaff744be67287be
Ben Coover <bcoover@ycp.edu>
2015-02-20 11:30:18 -0500

    -Square changed to circle -Circle moves 5x faster -Circle now Green

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -61,11 +61,11 @@ public class GameView extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g); // paint background
 		
-		g.setColor(Color.RED);
+		g.setColor(Color.GREEN);
 
 		Square square = model.getSquare();
 		
-		g.fillRect((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
+		g.fillOval((int) square.getX(), (int) square.getY(), (int) square.getWidth(), (int) square.getHeight());
 	}
 	
 	public static void main(String[] args) {
@@ -79,8 +79,8 @@ public class GameView extends JPanel {
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(80.0);
-				square.setHeight(80.0);
+				square.setWidth(40.0);
+				square.setHeight(40.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
